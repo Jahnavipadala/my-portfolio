9cdef916a4e908aca6893a3b12368964053eea57
TagoorJ <jayamangalatagore@gmail.com>
2025-12-13 18:47:17 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,7 @@ function App() {
           <div className="flex justify-between h-16">
             <div className="flex items-center">
               <span className="text-xl font-bold bg-gradient-to-r from-cyan-400 via-blue-400 to-purple-400 text-transparent bg-clip-text animate-gradient">Hello! 👋 
-                I’m Tagoor. Thanks for stopping by explore my work below.</span>
+                I’m Tagoor. Explore my work below.</span>
             </div>
 
             <div className="flex items-center sm:hidden">
